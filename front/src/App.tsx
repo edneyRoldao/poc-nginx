ff1b1690ee4f219f015ae65feb896eee422999a8
edneyroldao <edneyroldao@gmail.com>
2025-06-06 10:55:48 -0300

    test

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,26 @@
-// import { useState, useEffect } from 'react'
-// import axios from './axios-config'
+import { useState, useEffect } from 'react'
+import axios from './axios-config'
 import './App.css'
 
 function App() {
-  // const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([])
 
-  // useEffect(() => {
-  //   axios.get('/contacts').then((res) => setUsers(res.data))
-  // }, [])
+  useEffect(() => {
+    axios.get('/contacts').then((res) => setUsers(res.data))
+  }, [])
 
   return (
     <>
       <h1>Vite + React - Test Nginx</h1>
       <div className="card">
         <h2>Lista de Usuários - API Attack on Titan</h2>
-        {/* <ul>
+        { <ul>
             {users.map(user => (
               <li key={user['id']}>
                 <strong>{user['name']}</strong> - {user['phone']}
               </li>
             ))}
-          </ul> */}
+          </ul> }
       </div>
     </>
   )
--- startup.sh
+++ startup.sh
@@ -1,2 +1,33 @@
+echo "##########################################################"
+echo ""
+echo "INICIANDO SCRIPT DE ATUALIZACAO BACK e FRONT - nginx poc"
+echo ""
+echo "##########################################################"
+
 sudo cd ~/workspace/projects/poc-nginx/front
+
+echo ">>>>> parando os o container do frontend - nginx poc"
+sudo docker compose down
+sudo sleep 10
+
+echo ">>>>> recriando container frontend - nginx poc"
 sudo docker-compose up --build -d
+
+sudo cd ~/workspace/projects/poc-nginx/back
+
+echo ">>>>> parando as instancias do backend - nginx poc"
+sudo docker compose down
+sudo sleep 10
+
+echo ">>>>> recriando as instancias do backend - nginx poc"
+sudo docker-compose up --build -d
+sudo sleep 10
+
+echo ">>>>> atualizando conteudo estatico do frontend no nginx - nginx poc"
+sudo mv ~/nginx-static-apps/nginx_poc_frontend/* /usr/share/nginx/html
+
+echo ">>>>> atualizando arquivo nginx.conf - nginx poc"
+sudo cp ~/workspace/projects/poc-nginx/nginx.conf /etc/nginx
+
+echo ">>>>> recarregando NGINX - nginx poc"
+sudo nginx -s reload
