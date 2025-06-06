336ea13e2028c874d4a4972bc367d8599481d864
edneyroldao <edneyroldao@gmail.com>
2025-06-06 12:42:34 -0300

    test

--- startup.sh
+++ startup.sh
@@ -16,26 +16,26 @@ echo "---------------------------------"
 echo ">>>>> recriando container frontend - nginx poc <<<<<"
 echo "---------------------------------"
 cd ~/workspace/projects/poc-nginx/front
-sudo docker-compose down
-sudo docker rm nginx-poc-front
-sudo docker rmi nginx-poc-front-img
-sudo docker-compose up --build -d
+docker-compose down
+docker rm nginx-poc-front
+docker rmi nginx-poc-front-img
+docker-compose up --build -d
 
-sudo sleep 300
+sleep 300
 
 echo ""
 echo "---------------------------------"
 echo ">>>>> atualizando conteudo estatico do frontend no nginx - nginx poc <<<<<"
 echo "---------------------------------"
-sudo cp -r ~/nginx-static-apps/nginx_poc_frontend/* /usr/share/nginx/html
-sudo cp ~/workspace/projects/poc-nginx/nginx.conf /etc/nginx
-sudo nginx -s reload
+cp -r ~/nginx-static-apps/nginx_poc_frontend/* /usr/share/nginx/html
+cp ~/workspace/projects/poc-nginx/nginx.conf /etc/nginx
+nginx -s reload
 
 echo ""
 echo "---------------------------------"
 echo ">>>>> recriando as instancias do backend - nginx poc <<<<<"
 echo "---------------------------------"
 cd ~/workspace/projects/poc-nginx/back
-sudo docker-compose down
-sudo docker rmi nginx-poc-back
-sudo docker-compose up --build -d
+docker-compose down
+docker rmi nginx-poc-back
+docker-compose up --build -d
