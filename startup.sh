05b71fba328b9e55319c0bf81ef4204c7f47ac72
edneyroldao <edneyroldao@gmail.com>
2025-06-06 11:52:28 -0300

    test

--- startup.sh
+++ startup.sh
@@ -26,11 +26,11 @@ echo ">>>>> recriando as instancias do backend - nginx poc"
 sudo docker-compose up --build -d
 sudo sleep 10
 
-echo ">>>>> atualizando conteudo estatico do frontend no nginx - nginx poc"
-sudo mv ~/nginx-static-apps/nginx_poc_frontend/* /usr/share/nginx/html
+# echo ">>>>> atualizando conteudo estatico do frontend no nginx - nginx poc"
+# sudo mv ~/nginx-static-apps/nginx_poc_frontend/* /usr/share/nginx/html
 
-echo ">>>>> atualizando arquivo nginx.conf - nginx poc"
-sudo cp ~/workspace/projects/poc-nginx/nginx.conf /etc/nginx
+# echo ">>>>> atualizando arquivo nginx.conf - nginx poc"
+# sudo cp ~/workspace/projects/poc-nginx/nginx.conf /etc/nginx
 
-echo ">>>>> recarregando NGINX - nginx poc"
-sudo nginx -s reload
+# echo ">>>>> recarregando NGINX - nginx poc"
+# sudo nginx -s reload
